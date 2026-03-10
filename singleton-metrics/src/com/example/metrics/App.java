348ed77f4530359e592c075389b3bbc6e8dbb70d
Arman190205 <armanbarbhuiya1902@gmail.com>
2026-03-10 14:37:05 +0530

    updated

--- singleton-metrics/src/com/example/metrics/App.java
+++ singleton-metrics/src/com/example/metrics/App.java
@@ -4,15 +4,25 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
+        String propsPath = "metrics.properties";
+
         MetricsLoader loader = new MetricsLoader();
+        MetricsRegistry loaded = loader.loadFromFile(propsPath);
 
-        MetricsRegistry registry =
-                loader.loadFromFile("metrics.properties");
+        // In a correct design, loader should populate the SAME singleton instance.
+        MetricsRegistry global = MetricsRegistry.getInstance();
 
-        registry.increment("REQUESTS_TOTAL");
-        registry.increment("REQUESTS_TOTAL");
-        registry.increment("DB_ERRORS");
+        System.out.println("Loaded registry instance  : " + System.identityHashCode(loaded));
+        System.out.println("Global registry instance  : " + System.identityHashCode(global));
 
-        System.out.println("All Metrics: " + registry.getAll());
+        global.increment("REQUESTS_TOTAL");
+        System.out.println("\nREQUESTS_TOTAL = " + global.getCount("REQUESTS_TOTAL"));
+
+        System.out.println("\nAll counters:");
+        for (Map.Entry<String, Long> e : global.getAll().entrySet()) {
+            System.out.println("  " + e.getKey() + " = " + e.getValue());
+        }
+
+        System.out.println("\nTIP: Run ConcurrencyCheck / ReflectionAttack / SerializationCheck for validations.");
     }
 }
